4dbb486d49bad519aa03600a9162876fbcf1ce42
Balu Praveen Datty <dev.badatt@gmail.com>
2023-12-27 22:36:47 +0530

    Initial commit

---
+++ packages/web/src/index.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <h1>Building a fullstack TypeScript project with Turbo repo</h1>
+      <h2>Workspaces</h2>
+    </StrictMode>
+  );
+};
+
+const app = document.querySelector("#app");
+if (app) createRoot(app).render(<App />);
