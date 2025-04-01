076e9e446a628895c2c91f4c6e9aa524134f716d
badatt <dev.badatt@gmail.com>
2025-04-01 11:08:41 +0530

    with api base

--- infra/stack/backend.ts
+++ infra/stack/backend.ts
@@ -1,12 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import {Bucket} from 'aws-cdk-lib/aws-s3';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const s3 = new Bucket(this, 'LinkStore', {
-    });
   }
 }
