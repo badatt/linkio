769bfd43f893fe778edcb802aad8b7d04b10fe25
badatt <dev.badatt@gmail.com>
2025-04-05 14:17:55 +0530

    output

--- infra/src/context.ts
+++ infra/src/context.ts
@@ -35,7 +35,7 @@ export class Context {
     }
 
     public out(scope: Construct, name: string, value: string): void {
-        new CfnOutput(scope, `${this.props.appName}${name}`, {
+        new CfnOutput(scope, `${this.props.appName}${name}Out`, {
             value: value,
         }); 
     }
