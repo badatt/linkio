fc00d4fc9886b17156fd0697e5caa0636f79ca87
badatt <dev.badatt@gmail.com>
2025-04-05 20:57:31 +0530

    parameterized deployment

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <StrictMode>
       <h1>Building a fullstack TypeScript project</h1>
-      <h2>Workspaces</h2>
+      <h2>Workspaces sandbox</h2>
     </StrictMode>
   );
 };
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isSandbox = process.env.BUILD_TARGET === 'sandbox';
+
 export default defineConfig({
+  base: isSandbox ? '/sbx/' : '/',
+  build: {
+    outDir: isSandbox ? 'dist/sbx' : 'dist',
+  },
   plugins: [react()],
 });
