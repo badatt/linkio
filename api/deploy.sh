82b5a120a544ba60acd160841e00d99743eab424
badatt <dev.badatt@gmail.com>
2025-04-06 21:36:15 +0530

    update function env variables

---
+++ api/deploy.sh
@@ -0,0 +1,21 @@
+echo "Updating backend services..."
+echo "Function is available at $API_FUNCTION_ARN"
+
+zip -rq build.zip ./*
+aws lambda update-function-code --function-name $API_FUNCTION_ARN --zip-file fileb://build.zip
+
+echo "Updating environment variables..."
+NEW_ENV_VARS_TO_ADD=$(cat <<EOF
+{
+  "APP_NAME": "$APP_NAME"
+}
+EOF
+)
+
+NEW_ENVVARS=$(
+  aws lambda get-function-configuration \
+    --function-name $API_FUNCTION_ARN \
+    --query "Environment.Variables | merge(@, \`$NEW_ENV_VARS_TO_ADD\`)"
+)
+echo "New environment variables: $NEW_ENVVARS"
+aws lambda update-function-configuration --function-name $API_FUNCTION_ARN --environment "{ \"Variables\": $NEW_ENVVARS }"
