007aadee0ca7079006320b233506f1f4dda266c1
badatt <dev.badatt@gmail.com>
2025-04-09 00:37:01 +0530

    deployment to another repo

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isSandbox = process.env.BUILD_TARGET === 'sandbox';
-
 export default defineConfig({
-  base: isSandbox ? '/sbx/' : '/',
   build: {
-    outDir: isSandbox ? 'dist/sbx' : 'dist',
+    outDir: 'dist',
   },
   plugins: [react()],
   server: {
