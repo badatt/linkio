98d60208d6efb949ee2865e0ec8c888783d9e6d3
Balu Praveen Datty <Balu.Datty@pepsico.com>
2025-05-02 10:51:09 +0530

    fallback index

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        fallback: 'index.html'
+      },
+    }
   },
   plugins: [react()],
   server: {
