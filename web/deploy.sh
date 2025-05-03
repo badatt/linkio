00ff9f733f8ff1ed1afdd41126308fe7463c7b3e
Balu Praveen Datty <Balu.Datty@pepsico.com>
2025-05-03 18:40:50 +0530

    fix: update deploy script to use aws s3 cp for index.html upload

--- web/deploy.sh
+++ web/deploy.sh
@@ -3,7 +3,7 @@
 echo "Updating frontend services..." 
 echo "Deploying to $APP_DEPLOYMENT_BUCKET_NAME and clearing CloudFront $APP_CLOUDFRONT_ID cache..."
 
-aws s3 cp --recursive ./index.html s3://$APP_DEPLOYMENT_BUCKET_NAME/index.html
+aws s3 cp ./index.html s3://$APP_DEPLOYMENT_BUCKET_NAME/index.html
 aws s3 sync --delete ./assets/. s3://$APP_DEPLOYMENT_BUCKET_NAME/assets/
 aws cloudfront create-invalidation --distribution-id $APP_CLOUDFRONT_ID --paths "/*"
 echo "✅ Frontend deployed successfully."
