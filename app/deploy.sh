ca8374f93fead1edaae84826c321262b93e5e2f0
Balu Praveen Datty <Balu.Datty@pepsico.com>
2025-05-29 00:22:09 +0530

    copy everything

--- app/deploy.sh
+++ app/deploy.sh
@@ -3,7 +3,6 @@
 echo "Updating frontend services..." 
 echo "Deploying to $APP_DEPLOYMENT_BUCKET_NAME and clearing CloudFront $APP_CLOUDFRONT_ID cache..."
 
-aws s3 cp ./index.html s3://$APP_DEPLOYMENT_BUCKET_NAME/index.html
-aws s3 sync --delete ./assets/. s3://$APP_DEPLOYMENT_BUCKET_NAME/assets/
+aws s3 cp . s3://$APP_DEPLOYMENT_BUCKET_NAME
 aws cloudfront create-invalidation --distribution-id $APP_CLOUDFRONT_ID --paths "/*"
 echo "✅ Frontend deployed successfully."
