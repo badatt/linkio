f2fa8f8ce60504609691152c7fd8d401b17d6888
badatt <dev.badatt@gmail.com>
2025-06-07 01:34:30 +0530

    refactor: simplify logger initialization by integrating it directly into Fastify configuration

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,10 @@
 import awsLambdaFastify from '@fastify/aws-lambda';
 import Fastify, { FastifyInstance } from 'fastify';
-import pino from 'pino';
 
 import { app, options } from './app.js';
 
-const logger = pino(
-  {
+const fastify: FastifyInstance = Fastify({
+  logger: {
     level: 'info',
     timestamp: false,
     formatters: {
@@ -21,10 +20,8 @@ const logger = pino(
     },
     messageKey: 'msg',
   },
-  pino.destination(1),
-);
+});
 
-const fastify: FastifyInstance = Fastify({ logger });
 fastify.register(app, options);
 
 const proxy = awsLambdaFastify(fastify);
