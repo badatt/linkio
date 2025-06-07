ac6d2a7509bdab2011c7fd9a9a3121d284d7c16c
badatt <dev.badatt@gmail.com>
2025-06-07 16:02:58 +0530

    refactor: update request ID generation in tracer plugin to use existing request ID

--- api/src/plugins/tracer.plugin.ts
+++ api/src/plugins/tracer.plugin.ts
@@ -1,15 +1,11 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import fp from 'fastify-plugin';
-import { randomUUID } from 'crypto';
 
 export default fp(async (fastify) => {
   fastify.addHook('onRequest', async (request: FastifyRequest, reply: FastifyReply) => {
-    const traceId = randomUUID();
-    request.id = traceId;
-    request.traceId = traceId;
     console.log(`Generated request id ${request.id}`);
+    request.traceId = request.id;
     reply.header('x-go-trace-id', request.id);
-    request.log = request.log.child({ traceId });
   });
 });
 
