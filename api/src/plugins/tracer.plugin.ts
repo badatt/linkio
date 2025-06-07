664b36c1b8130f14dd9b541697e60294e4411630
badatt <dev.badatt@gmail.com>
2025-06-07 16:58:23 +0530

    refactor: enhance logging structure in tracer plugin for better clarity and detail

--- api/src/plugins/tracer.plugin.ts
+++ api/src/plugins/tracer.plugin.ts
@@ -9,11 +9,39 @@ export default fp(async (fastify) => {
     request.log = fastify.log.child({
       reqId: traceId,
     });
-    request.log.info(request);
+    request.log.info({
+      ...request,
+      hostname: request.hostname,
+      host: request.host,
+      originalUrl: request.originalUrl,
+      from: request.headers.from,
+    });
   });
 
   fastify.addHook('onSend', async (request: FastifyRequest, reply: FastifyReply) => {
     reply.header('x-go-trace-id', request.id);
-    request.log.info(reply);
+    request.log.info({
+      where: 'onSend request log',
+      elapse: reply.elapsedTime,
+      statusCode: reply.statusCode,
+    });
+    reply.log.info({
+      where: 'onSend reply log',
+      elapse: reply.elapsedTime,
+      statusCode: reply.statusCode,
+    });
+  });
+
+  fastify.addHook('onResponse', async (request: FastifyRequest, reply: FastifyReply) => {
+    request.log.info({
+      where: 'onResponse request log',
+      elapse: reply.elapsedTime,
+      statusCode: reply.statusCode,
+    });
+    reply.log.info({
+      where: 'onResponse reply log',
+      elapse: reply.elapsedTime,
+      statusCode: reply.statusCode,
+    });
   });
 });
