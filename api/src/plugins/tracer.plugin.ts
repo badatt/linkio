55ecde684e78ee80271bbc8d02afb14e2448f185
badatt <dev.badatt@gmail.com>
2025-06-07 15:46:30 +0530

    refactor: unify request ID and trace ID generation in onRequest hook

--- api/src/plugins/tracer.plugin.ts
+++ api/src/plugins/tracer.plugin.ts
@@ -4,10 +4,12 @@ import { randomUUID } from 'crypto';
 
 export default fp(async (fastify) => {
   fastify.addHook('onRequest', async (request: FastifyRequest, reply: FastifyReply) => {
-    request.id = randomUUID();
+    const traceId = randomUUID();
+    request.id = traceId;
+    request.traceId = traceId;
     console.log(`Generated request id ${request.id}`);
-    request.traceId = request.id;
     reply.header('x-go-trace-id', request.id);
+    request.log = request.log.child({ traceId });
   });
 });
 
