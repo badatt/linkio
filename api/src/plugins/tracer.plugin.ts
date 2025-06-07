6292ebf46017b81225c8f207c5a65b0f4e5b5aca
badatt <dev.badatt@gmail.com>
2025-06-07 16:35:50 +0530

    refactor: simplify logging in tracer plugin and update slug generation log in create handler

--- api/src/plugins/tracer.plugin.ts
+++ api/src/plugins/tracer.plugin.ts
@@ -3,21 +3,17 @@ import fp from 'fastify-plugin';
 import { randomUUID } from 'crypto';
 
 export default fp(async (fastify) => {
-  fastify.addHook('onRequest', async (request: FastifyRequest, reply: FastifyReply) => {
+  fastify.addHook('onRequest', async (request: FastifyRequest) => {
     const traceId = randomUUID();
     request.id = traceId;
-    request.traceId = request.id;
     request.log = fastify.log.child({
       reqId: traceId,
-      traceId: traceId,
     });
-    request.log.info(`Generated request id ${request.id}`);
+    request.log.info(request);
+  });
+
+  fastify.addHook('onSend', async (request: FastifyRequest, reply: FastifyReply) => {
     reply.header('x-go-trace-id', request.id);
+    request.log.info(reply);
   });
 });
-
-declare module 'fastify' {
-  interface FastifyRequest {
-    traceId: string;
-  }
-}
