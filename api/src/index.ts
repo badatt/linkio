f8399d4b80046e9fa9e72d1ed99e622e62502715
badatt <dev.badatt@gmail.com>
2025-06-07 13:12:04 +0530

    refactor: enhance logger configuration by restructuring formatters and serializers

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,15 +4,54 @@ import Fastify, { FastifyInstance } from 'fastify';
 import { app, options } from './app.js';
 
 const fastify: FastifyInstance = Fastify({
+  genReqId: (req) => {
+    return req.traceID;
+  },
   logger: {
     level: 'info',
-    timestamp: false,
-    redact: ['req.headers.authorization'],
-    genReqId(req): string {
-      return req.traceId;
+    messageKey: 'msg',
+    formatters: {
+      level(label) {
+        return { level: label };
+      },
+      bindings() {
+        return {};
+      },
+      log(object) {
+        return {
+          traceId: object.reqId ?? object.traceId,
+          message: object.msg,
+          ...object,
+        };
+      },
     },
+    timestamp: false,
+    serializers: {
+      req(req) {
+        return {
+          traceId: req.id,
+          method: req.method,
+          url: req.url,
+        };
+      },
+      res(res) {
+        return {
+          statusCode: res.statusCode,
+          status: res.status,
+          elapsed: res.elapsedTime,
+        };
+      },
+      err(err) {
+        return {
+          statusCode: err.statusCode,
+          message: err.message,
+          type: err.code,
+          stack: err.stack ?? '',
+        };
+      },
+    },
+    redact: ['req.headers.authorization'],
   },
-  disableRequestLogging: true,
 });
 
 fastify.register(app, options);
